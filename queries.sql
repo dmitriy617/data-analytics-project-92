f412f8fed5bed887861b5a182089074d92f704c3
dmitriy617 <137271446+dmitriy617@users.noreply.github.com>
2024-02-13 18:32:30 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ from customers c ;
 select
 concat_ws(' ', first_name,last_name) as name,
 count(sales_id) as operations,
-ROUND(SUM(s.quantity*p.price),4) as income
+ROUND(SUM(s.quantity*p.price),0) as income
 from sales s  
 join products p 
 on s.product_id = p.product_id
@@ -39,13 +39,13 @@ order by average_income;
 select
 concat_ws(' ', e.first_name,e.last_name)as name,
 case to_char(s.sale_date, 'id') 
-when '1' then 'monday'::varchar(6)
-when '2' then 'tuesday'::varchar(7)
-when '3' then 'wednesday'::varchar(9)
-when '4' then 'thursday'::varchar(8)
-when '5' then 'friday'::varchar(6)
-when '6' then 'saturday'::varchar(8)
-when '7' then 'sunday'::varchar(6)
+when '1' then 'monday'
+when '2' then 'tuesday'
+when '3' then 'wednesday'
+when '4' then 'thursday'
+when '5' then 'friday'
+when '6' then 'saturday'
+when '7' then 'sunday'
 end as weekday,
 round(sum(s.quantity*p.price),0) as income
 from sales s  
@@ -79,7 +79,7 @@ order by age_category;
 select 
 to_char(s.sale_date, 'YYYY-MM') as date,
 count(distinct c.customer_id)as total_customers,
-round(SUM(s.quantity*p.price),4) as income
+round(SUM(s.quantity*p.price),0) as income
 from customers c
 join sales s
 on c.customer_id = s.customer_id

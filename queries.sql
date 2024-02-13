84f3e9e0944543c36f4a955332b3271dbd84e736
dmitriy617 <137271446+dmitriy617@users.noreply.github.com>
2024-02-13 12:59:34 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
+/*запрос считает общее количество покупателей
 select
-COUNT(customer_id) as customers_count
+COUNT(distinct customer_id) as customers_count
 from customers c ;
-/* запрос считает общее количество покупателей
 
-
+/* запрос считает 10 продавцов с наибольшей выручкой
 select
 concat_ws(' ', first_name,last_name) as name,
 count(sales_id) as operations,
@@ -17,8 +17,8 @@ group by concat_ws(' ', first_name,last_name)
 order by income
 limit 10;
 
-/* запрос считает 10 продавцов с наибольшей выручкой
 
+/* запрос считает выручку и сортирует ее от меньшего к большему
 select
 concat_ws(' ', first_name,last_name) as name,
 round(avg(s.quantity*p.price),0) as average_income
@@ -34,19 +34,18 @@ join products pp
 on ss.product_id = pp.product_id)
 order by average_income;
 
-/* запрос считает выручку и сортирует ее от меньшего к большему
 
-
+/* запрос считает выручку и сортирует ее по каждому продавцу и дню недели
 select
 concat_ws(' ', e.first_name,e.last_name)as name,
-case extract(isodow from s.sale_date)
-when 1 then 'monday'
-when 2 then 'tuesday'
-when 3 then 'wednesday'
-when 4 then 'thursday'
-when 5 then 'friday'
-when 6 then 'saturday'
-when 7 then 'sunday'
+case to_char(s.sale_date, 'id') 
+when '1' then 'monday'::varchar(6)
+when '2' then 'tuesday'::varchar(7)
+when '3' then 'wednesday'::varchar(9)
+when '4' then 'thursday'::varchar(8)
+when '5' then 'friday'::varchar(6)
+when '6' then 'saturday'::varchar(8)
+when '7' then 'sunday'::varchar(6)
 end as weekday,
 round(sum(s.quantity*p.price),0) as income
 from sales s  
@@ -54,32 +53,29 @@ join products p
 on s.product_id = p.product_id
 join employees e 
 on s.sales_person_id=e.employee_id
-group by concat_ws(' ', first_name,last_name), extract(isodow from s.sale_date)
-order by extract(isodow from s.sale_date),name ;
+group by concat_ws(' ', first_name,last_name), to_char(s.sale_date, 'id')
+order by to_char(s.sale_date, 'id'), name;
 
-
-/* запрос считает выручку и сортирует ее по каждому продавцу и дню недели
-
-
+/* запрос считает количество покупателей по разным возрастным группам(16-25, 26-40, 40+)
 select
 case
 	when age between 16 and 25 then '16-25'
 	when age between 26 and 40 then '26-40'
-	when age >40 then '>40'
+	when age >40 then '40+'
 end age_category,
 count(customer_id)
 from customers c
 group by case
 	when age between 16 and 25 then '16-25'
 	when age between 26 and 40 then '26-40'
-	when age >40 then '>40' 
+	when age >40 then '40+' 
 	end
 order by age_category;
 
 
-/* запрос считает количество покупателей по разным возрастным группам(16-25, 26-40, 40+)
 
 
+/* запрос считает количество уникальных покупателей и вырочку, которую они принесли
 select 
 to_char(s.sale_date, 'YYYY-MM') as date,
 count(distinct c.customer_id)as total_customers,
@@ -92,8 +88,8 @@ on s.product_id = p.product_id
 group by to_char(s.sale_date, 'YYYY-MM')
 ;
 
-/* запрос считает количество уникальных покупателей и вырочку, которую они принесли
 
+/* запрос сортирует покупателей, первая покупка которых была в ходе акции(стоимость равна 0)
 select 
 concat_ws(' ', c.first_name,c.last_name) as customer,
 min(s.sale_date) as sale_date,
@@ -109,7 +105,6 @@ where p.price = 0
 group by concat_ws(' ', c.first_name,c.last_name), concat_ws(' ', e.first_name,e.last_name), c.customer_id
 order by c.customer_id;
 
-/* запрос сортирует покупателей, первая покупка которых была в ходе акции(стоимость равна 0)
 
  
 
